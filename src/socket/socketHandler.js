4fdb0895a4dd903663801938a1444d3e08e20421
MarcelloBelem <devmarcellobel@gmail.com>
2026-01-20 23:49:54 -0300

    Update Socket: Change in welcome

--- src/socket/socketHandler.js
+++ src/socket/socketHandler.js
@@ -2,7 +2,10 @@ export default (io) => {
   io.on("connection", (socket) => {
     console.log(`Cliente conectado: ${socket.id}`);
     //Mensagem de Boas-Vindas
-    socket.emit("server:welcome", { msg: "Seja Bem-Vindo", id: socket.id });
+    socket.emit("server:welcome", {
+      msg: "Convide um amigo e interaja em tempo real! 🚀 (O histórico apaga ao reiniciar)",
+      id: socket.id,
+    });
 
     //Teste de Ping Pong
     socket.on("client:ping", (data) => {
